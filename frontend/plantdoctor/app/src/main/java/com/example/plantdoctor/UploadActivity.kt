c41de130bf08027a4a18f709d7226207ee4a4b8a
anan940723 <11436023@ntub.edu.tw>
2026-05-28 22:28:43 +0800

    把測試用程式註解掉

--- frontend/plantdoctor/app/src/main/java/com/example/plantdoctor/UploadActivity.kt
+++ frontend/plantdoctor/app/src/main/java/com/example/plantdoctor/UploadActivity.kt
@@ -219,7 +219,7 @@ class UploadActivity : AppCompatActivity() {
 
                     // 🌟 【就是這裡！】壓縮成功後，順手把這個絕對路徑塞給假 API 的小倉庫！
                     // 這樣等一下 confirmDiary 儲存時，才能百分之百抓到這張剛拍好的圖！
-                    PlantApiService.latestMockImageUri = compressedFile.absolutePath
+                    //PlantApiService.latestMockImageUri = compressedFile.absolutePath
 
                     Glide.with(this)
                         .load(compressedUri)
@@ -231,7 +231,7 @@ class UploadActivity : AppCompatActivity() {
                     selectedImageUri = uri
 
                     // 🌟 【安全防禦】如果壓縮失敗退回原 Uri，也同步把原本的字串塞給假 API
-                    PlantApiService.latestMockImageUri = uri.toString()
+                    //PlantApiService.latestMockImageUri = uri.toString()
 
                     Glide.with(this)
                         .load(uri)
